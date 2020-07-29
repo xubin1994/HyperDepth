5419c39d2930bc7eb262cf2c70117359268fa5ae
eoz-imaginary <zoe.riell@gmail.com>
2020-07-29 15:23:14 -0600

    working on GTgen

---
+++ HyperDepth/GTgen.py
@@ -0,0 +1,55 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+import utilities as utils
+
+data_path = "C:\\Users\\Zoe\\Documents\\Thesis\\Test Data\\playground-hyperdepth-master-images"
+save_path = "C:\\Users\\Zoe\\Documents\\Thesis\\Test Data\\playground-hyperdepth-GT"
+ref_path = "" #not sure what ref image will be yet
+
+max_displacement = 128
+block_size = 21
+
+# make sure save path exists
+try:
+    os.mkdir( save_path )
+except:
+    pass
+
+# read in ref image
+img_ref = cv2.imread(ref_path, 0)
+
+# get list of images
+img_list = utils.list_images(data_path)
+
+# set up cv2's block matching algorithm 
+
+bm_alg = cv2.StereoBM_create(numDisparities = None, blockSize = block_size)
+bm_alg.setMinDisparity(0)
+
+# loop over all images & generate disparity map
+
+for idx, entry in img_list.iterrows():
+    print("idx = " + idx)
+    img = cv2.imread(entry['filename'], 0)
+    disp_map = bm_alg.compute(img, img_ref)
+
+    # save disparity map to disk
+    disp_map[disp_map<0] = 0 # remove any <0 values
+    file_name = "%s/%s.png" % (save_path,entry['hash'])
+    cv2.imread(file_name, disp_map.astype(np.uint16)) # 16 bit for now, might change
+
+    # plot disparity maps every 25 images
+    if idx % 25 == 0:
+        plt.figure(1)
+        plt.imshow(img)
+        plt.show(block = False)
+        plt.pause(0.5)
+
+        plt.figure(2)
+        plt.imshow(disp_map/16.0)
+        plt.clim(-16,256/2)
+        plt.show(block = False )
+        plt.pause(0.5)
